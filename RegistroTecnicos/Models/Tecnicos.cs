45534887eb048490df2c65a72f165d4839c9c6e1
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2025-01-19 02:45:48 -0400

    Se creo la clase Tecnicos

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Tecnicos
+{
+    [Key]
+    [Range(1, int.MaxValue, ErrorMessage = "El ID debe ser mayor o igual que 1.")]
+    public int TecnicoId { get; set; }
+    [Required(ErrorMessage = "Este campo es requerido")]
+    public string? Nombre { get; set; }
+    [Range (0.01, double.MaxValue, ErrorMessage = "El campo sueldo por hora debe ser mayor que cero.")]
+    public int SueldoHora { get; set; }
+}       

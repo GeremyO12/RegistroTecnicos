b69c4d9b3f57c3256687844c0a2e0df0d4e8767c
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2025-01-19 02:47:48 -0400

    Se agregaron las inyecciones

--- RegistroTecnicos/Program.cs
+++ RegistroTecnicos/Program.cs
@@ -1,10 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Radzen;
 using RegistroTecnicos.Components;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+//builder.Services.AddSingleton<Radzen.NotificationService>();
+builder.Services.AddBlazorBootstrap();
+
+//Obtenemos el ConStr para usarlo en el contexto
+var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
+
+//Agregamos el contexto al builder con el ConStr
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
+
+//Inyeccion del service
+builder.Services.AddScoped<TecnicoService>();
 
 var app = builder.Build();
 

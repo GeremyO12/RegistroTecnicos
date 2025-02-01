506115294319f6c145e7f74387d307f8f41e1df6
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2025-02-01 05:11:30 -0400

    Se agrego el contexto Clientes

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -7,5 +7,7 @@ namespace RegistroTecnicos.DAL
     {
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
         public DbSet<Tecnicos> Tecnicos { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Tickets> Tickets { get; set; }
     }
 }
